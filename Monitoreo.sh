15c955fe3140e7c68c5478da50ba281ca8bd710e
Alexa LM <135302021+Alexalopezm@users.noreply.github.com>
2023-09-19 12:06:09 -0600

    Version final del codigo

--- Monitoreo.sh
+++ Monitoreo.sh
@@ -1,39 +1,25 @@
 #!/bin/bash
 
-# Verifica si se proporcionan los argumentos correctos.
+# Verifica que se proporcionen los argumentos requeridos
 if [ $# -ne 2 ]; then
     echo "Error, ingrese el nombre del proceso y comando para ejecutar"
     echo "Uso: $0 <nombre_del_proceso> <comando_para_ejecutar>"
     exit 1
 fi
 
-# Almacena el nombre del proceso y el comando para ejecutarlo.
-process_name="$1"
-command_to_run="$2"
+nombre_proceso="$1"
+comando="$2"
 
-# Función para verificar y reiniciar el proceso.
-monitor_process() {
-    while true; do
-        # Verifica si el proceso está en ejecución.
-        if pgrep -x "$process_name" >/dev/null; then
-            echo "El proceso '$process_name' está en ejecución."
-        else
-            echo "El proceso '$process_name' no está en ejecución. Se volverá a iniciar."
-            # Inicia el proceso nuevamente.
-            eval "$command_to_run" &
-        fi
+while true; do
+  # Verificar si el proceso está en ejecución
+  if pgrep -x "$nombre_proceso" >/dev/null; then
+    echo "El proceso '$nombre_proceso' está en ejecución."
+  else
+    echo "El proceso '$nombre_proceso' ha terminado. Reiniciando..."
+    # Ejecutar el comando para reiniciar el proceso
+    $comando &
+  fi
 
-        # Espera un tiempo antes de volver a verificar.
-        sleep 5  # Puedes ajustar el intervalo de verificación según tus necesidades.
-    done
-}
-
-# Inicia el monitoreo del proceso en segundo plano.
-monitor_process &
-
-# Muestra un mensaje informativo.
-echo "Monitoreando el proceso '$process_name'. Presiona Ctrl+C para detener el monitoreo."
-
-# Espera hasta que se presione Ctrl+C para detener el monitoreo.
-trap 'echo "Deteniendo el monitoreo."; exit' SIGINT
-wait
+  # Intervalo de tiempo para revisar el estado del proceso (en segundos)
+  sleep 5
+done
